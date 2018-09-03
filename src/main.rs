36e8e9c17fb2d3293a61aaf125f53faf7ef9e74a
AuroransSolis <primalucegd@gmail.com>
2018-09-03 01:49:29 -0700

    Made improvements to the iterator. It's now faster than testing (on the computer I've tested on thus far, that is).

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,62 @@
-use std::sync::{mpsc::{Sender, Receiver}, Arc, Mutex};
+use std::sync::{Arc, Mutex};
 use std::thread;
 
-const MAX_X: u64 = 1_000_000;
+const MAX_X: u64 = 1_000;
 const BUFFER_SIZE: usize = 1_000_000;
 
 fn main() {
-    let iter = (2..MAX_X).map(|x| x * x)
-        .flat_map(move |x| (2..x).flat_map(move |y| (2..x - y).filter(move |&z| z != y)
-            .map(move |z| (x, y, z))))
-        .filter(|&(x, y, z)| all_valid(x, y, z));
+    let iter = (2..MAX_X).map(|x| x * x).filter(|&x| x % 24 == 1)
+        .flat_map(move |x| (2..x / 24).map(|y| 24 * y)
+            .flat_map(move |y| (2..(x - y) / 24).map(|z| z * 24).filter(move |&z| z != y)
+                .map(move |z| (x, y, z))));
     let arc_mut_iter = Arc::new(Mutex::new(iter));
     let mut handles = Vec::new();
     let main_start = std::time::SystemTime::now();
-    for i in 0..2 {
+    for i in 0..3 {
         let am_iter = Arc::clone(&arc_mut_iter);
         let builder = thread::Builder::new().stack_size(24 * BUFFER_SIZE);
         let test_handle = builder.spawn(move || {
             let mut internal_buffer: [(u64, u64, u64); BUFFER_SIZE] =
                 [(0, 0, 0); BUFFER_SIZE];
-            for _ in 0..4 {
+            let mut rerun = false;
+            'thread_loop: loop {
                 {
                     let mut t_iter = am_iter.lock().unwrap();
-                    println!("({}) Starting to pull numbers from the iterator.", i);
-                    let start = std::time::SystemTime::now();
+                    //println!("({}) Starting to pull numbers from the iterator.", i);
+                    //let start = std::time::SystemTime::now();
                     for pos in 0..BUFFER_SIZE {
-                        internal_buffer[pos] = t_iter.next().expect("Iterator has no more values.");
+                        internal_buffer[pos] = if let Some(trip) = t_iter.next() {
+                            trip
+                        } else {
+                            //println!("Breaking for loop!");
+                            //println!("Time taken before break: {:?}", start.elapsed().unwrap());
+                            rerun = true;
+                            break 'thread_loop;
+                        }
                     }
-                    println!("({}) Refilled the buffer with data. Time taken: {:?}", i,
-                             start.elapsed().unwrap());
+                    //println!("({}) Refilled the buffer with data. Time taken: {:?}", i,
+                    //         start.elapsed().unwrap());
                 }
-                println!("({}) Starting to test data.", i);
-                let test_start = std::time::SystemTime::now();
+                //println!("({}) Starting to test data.", i);
+                //let test_start = std::time::SystemTime::now();
                 for trip in internal_buffer.iter() {
                     if test_squares(trip.0, trip.1, trip.2) {
                         println!("haha what the fuck? {:?}", trip);
                     }
                 }
-                println!("({}) Completed testing. Time taken: {:?}", i, test_start.elapsed().unwrap());
+                //println!("({}) Completed testing. Time taken: {:?}", i,
+                //         test_start.elapsed().unwrap());
+            }
+            if rerun {
+                //println!("({}) [Final!] Starting to test data.", i);
+                //let test_start = std::time::SystemTime::now();
+                for trip in internal_buffer.iter() {
+                    if test_squares(trip.0, trip.1, trip.2) {
+                        println!("haha what the fuck? {:?}", trip);
+                    }
+                }
+                //println!("({}) [Final!] Completed testing. Time taken: {:?}", i,
+                //         test_start.elapsed().unwrap());
             }
         }).unwrap();
         handles.push(test_handle);
@@ -45,12 +65,6 @@ fn main() {
         handle.join().unwrap();
     }
     println!("Total time: {:?}", main_start.elapsed().unwrap());
-}
-
-fn all_valid(x: u64, y: u64, z: u64) -> bool {
-    (x + y) % 24 == 1 && (x - y - z) % 24 == 1 && (x + z) % 24 == 1
-        && (x - y + z) % 24 == 1 && x % 24 == 1 && (x + y - z) % 24 == 1
-        && (x - z) % 24 == 1 && (x + y + z) % 24 == 1 && (x - y) % 24 == 1
 }
 
 // Using method found on SE
